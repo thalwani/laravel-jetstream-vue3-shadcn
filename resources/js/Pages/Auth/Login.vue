86e36153dc928cde7eb6251c9c4864d394fb7894
Thalwani <toufic.halwani@gmail.com>
2024-11-07 05:33:06 +0100

    :heavy_plus_sign: Social Login Buttons

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -46,6 +46,33 @@ const submit = () => {
         >
             {{ status }}
         </div>
+        <div class="grid grid-cols-2 gap-6">
+            <Button variant="outline">
+                <svg
+                    class="mr-2 h-6 w-6"
+                    x="0px"
+                    y="0px"
+                    width="100"
+                    height="100"
+                    viewBox="0 0 30 30"
+                >
+                    <path
+                        d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"
+                    ></path>
+                </svg>
+                Facebook
+            </Button>
+            <Button variant="outline">
+                <svg role="img" viewBox="0 0 24 24" class="mr-2 h-4 w-4">
+                    <path
+                        fill="currentColor"
+                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                    ></path>
+                </svg>
+                Google
+            </Button>
+        </div>
+        <Separator :label="$t('Or continue with')" />
         <form @submit.prevent="submit" class="grid gap-4">
             <div class="grid gap-2">
                 <Label for="email">{{ $t("Email") }}</Label>
@@ -88,7 +115,7 @@ const submit = () => {
                         <span
                             class="ms-2 text-sm text-gray-600 dark:text-gray-400"
                         >
-                            {{ $t('Remember me') }}
+                            {{ $t("Remember me") }}
                         </span>
                     </label>
                 </div>
@@ -102,22 +129,11 @@ const submit = () => {
                 {{ $t("Log in") }}
             </Button>
         </form>
-        <Separator class="my-4" label="Ou" />
-        <Button variant="outline" class="w-full">
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="22" height="22" viewBox="0 0 48 48">
-            <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
-          </svg>
-          {{ $t('Log in with Google') }} 
-        </Button>
-        <Button variant="outline" class="w-full">
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
-            <linearGradient id="Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1" x1="9.993" x2="40.615" y1="9.993" y2="40.615" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#2aa4f4"></stop><stop offset="1" stop-color="#007ad9"></stop></linearGradient><path fill="url(#Ld6sqrtcxMyckEl6xeDdMa_uLWV5A9vXIPu_gr1)" d="M24,4C12.954,4,4,12.954,4,24s8.954,20,20,20s20-8.954,20-20S35.046,4,24,4z"></path><path fill="#fff" d="M26.707,29.301h5.176l0.813-5.258h-5.989v-2.874c0-2.184,0.714-4.121,2.757-4.121h3.283V12.46 c-0.577-0.078-1.797-0.248-4.102-0.248c-4.814,0-7.636,2.542-7.636,8.334v3.498H16.06v5.258h4.948v14.452 C21.988,43.9,22.981,44,24,44c0.921,0,1.82-0.084,2.707-0.204V29.301z"></path>
-          </svg>
-          {{ $t('Log in with Facebook') }} 
-        </Button>
-        <div class="mt-4 text-center text-sm">
-            {{ $t('Dont Have An Account?') }}
-            <Link :href="route('register')" class="underline">{{ $t('Register') }}</Link>
+        <div class="text-center text-sm">
+            {{ $t("Dont Have An Account?") }}
+            <Link :href="route('register')" class="underline">{{
+                $t("Register")
+            }}</Link>
         </div>
     </AuthenticationCard>
 </template>
